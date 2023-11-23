ef4d225720349a79688601667cd253e613900696
AHMAD BD <ahmad2135@gmail.com>
2023-11-23 09:31:43 +0300

    add models

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('throttle:api')->group(function () {
+    Route::get('/news', 'NewsController@index');
+});
